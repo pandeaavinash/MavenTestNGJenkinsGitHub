1b063443c348e6b4773404841e8ef79442120e8d
Avinash Pande <aavinashpande@gmail.com>
2018-02-10 15:43:44 +0530

    <apande> Initial Checkin

---
+++ MavenTestNGJenkinsGitHub/src/test/java/tests/FirstTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+  @Test
+  public void f() 
+  {
+	  System.out.println("Avinash");
+	  System.out.println("Avinash");
+	  System.out.println("Avinash");
+	  System.out.println("Avinash");
+  }
+}
